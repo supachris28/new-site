baaddc1b5aa0949ccd6c5a188e3869ca13d746ac
Chris Key <chrisk@packt.com>
2019-10-11 09:09:29 +0100

    Added course component

--- src/app/viewer/viewer-routing.module.ts
+++ src/app/viewer/viewer-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreComponent } from './core/core.component';
 import { NewsComponent } from './news/news.component';
+import { CourseComponent } from './course/course.component';
 
 
 const routes: Routes = [
   { path: 'viewer', component: CoreComponent, children: [
     { path: 'news', component: NewsComponent },
+    { path: 'course', component: CourseComponent },
   ] }
 ];
 
--- src/app/viewer/viewer.module.ts
+++ src/app/viewer/viewer.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ViewerRoutingModule } from './viewer-routing.module';
 import { CoreComponent } from './core/core.component';
 import { NewsComponent } from './news/news.component';
+import { CourseComponent } from './course/course.component';
 
 
 @NgModule({
   declarations: [
     CoreComponent,
     NewsComponent,
+    CourseComponent,
   ],
   imports: [
     CommonModule,
