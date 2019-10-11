15c2a1480c181cd5eed3ebc2ca767a6349e6a79c
Chris Key <chrisk@packt.com>
2019-10-11 09:08:37 +0100

    Added empty news component

--- src/app/viewer/viewer-routing.module.ts
+++ src/app/viewer/viewer-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreComponent } from './core/core.component';
+import { NewsComponent } from './news/news.component';
 
 
 const routes: Routes = [
-  { path: 'viewer', component: CoreComponent },
+  { path: 'viewer', component: CoreComponent, children: [
+    { path: 'news', component: NewsComponent },
+  ] }
 ];
 
 @NgModule({
--- src/app/viewer/viewer.module.ts
+++ src/app/viewer/viewer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ViewerRoutingModule } from './viewer-routing.module';
 import { CoreComponent } from './core/core.component';
+import { NewsComponent } from './news/news.component';
 
 
 @NgModule({
   declarations: [
     CoreComponent,
+    NewsComponent,
   ],
   imports: [
     CommonModule,
